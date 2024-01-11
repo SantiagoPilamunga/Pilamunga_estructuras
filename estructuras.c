c8d0e83ec90a4c48f9fd4d884d32d1919101375c
Santiago <santiago.pilamunga@udla.edu.ec>
2024-01-11 16:33:52 -0500

    Inicio2

--- estructuras.c
+++ estructuras.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    
-    
+    struct alumno
+    {
+        int matricula;
+        char nombre[50];
+        char direccion[50];
+        char materia[50];
+        float nota;
+    };
+    struct alumno a1 = {100, "Juan Perez", "Quito", "Programcion", 8.8};
+
     return 0;
 }
